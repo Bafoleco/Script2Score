8a97934e76ced0e79d77723683036cbe1b93b823
wcroberson <wcr2023@stanford.edu>
2021-02-14 16:52:01 -0800

    code written to load data from csv into main

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
+import csv
 import math
 import numpy as np
-import tensorflow as tf
+#import tensorflow as tf
 
-print ("Hello, world!")
+print("Hello, world!")
 
+data_file = "movie_metadata.csv"
+X = []
+Y = []
+
+with open(data_file, 'r') as f:
+    csv_reader = csv.reader(f)
+    count = 0
+    for row in csv_reader:
+        try:
+            if "title" in row[0]:
+                continue
+            train_example = row[2:-2]
+            X.append(train_example)
+            train_output = row[-1]
+            Y.append(train_output)
+        except IndexError:
+            pass
+
+X = np.array(X).T.astype(np.float)
+Y = np.array(Y, ndmin=2).astype(np.float)
 
 #https://machinelearningmastery.com/tutorial-first-neural-network-python-keras/
